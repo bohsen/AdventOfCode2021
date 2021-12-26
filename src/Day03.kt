b7a1d572fcd5e266585b295483227277a9877ae2
bohsen <bohsen@gmail.com>
2021-12-26 21:51:36 +0100

    Part 1 of day3 solved

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,9 +1,29 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val length = input[0].length
+        val gamma = IntArray(length)
+        val epsilon = IntArray(length)
+
+        for(i in 0 until length) {
+            var x: Int = 0 // 0
+            var y: Int = 0 // 1
+            for(line in input) {
+                when(line[i].digitToInt()) {
+                    0 -> x++
+                    1 -> y++
+                }
+            }
+            if (x>y) gamma[i] = 0 else gamma[i] = 1
+            if (x>y) epsilon[i] = 1 else epsilon[i] = 0
+        }
+
+        return gamma.joinToString("").toInt(2)*epsilon.joinToString("").toInt(2)
     }
 
     fun part2(input: List<String>): Int {
+
 
         return 0
     }
